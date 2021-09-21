67076d18d381a764f38bd96e7c831de857c7de90
Magnus Gaustad Eriksson <magnus.eriksson@bouvet.no>
2021-09-21 13:16:23 +0200

    Lagt til ikon og knapper

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fallvilt/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -7,8 +8,59 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hello"),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Ola Nordmann"), actions: [
+        IconButton(
+          onPressed: () => print("go to profile"),
+          icon: const Icon(Icons.person, color: Colors.white),
+        ),
+      ]),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Image(image: AssetImage('assets/icons/fallvilt.png')),
+            const Text(
+              "Fallvilt",
+              style: TextStyle(fontSize: 30),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(250, 50), shape: const BeveledRectangleBorder()),
+                    onPressed: () => print("Ny"),
+                    icon: Icon(Icons.add),
+                    label: Text("Ny registrering"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(250, 50), shape: const BeveledRectangleBorder()),
+                      onPressed: () => print("Ny"),
+                      icon: Icon(Icons.text_snippet),
+                      label: Text("Gå til utkast")),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(250, 50), shape: const BeveledRectangleBorder()),
+                    onPressed: () => print("Ny"),
+                    icon: Icon(Icons.send),
+                    label: Text("Gå til overførte"),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
