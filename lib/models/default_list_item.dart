280160f161ceaf449a6edad86445a74f31f97f40
Magnus Gaustad Eriksson <magnus.eriksson@bouvet.no>
2021-09-21 11:28:24 +0200

    Lagt til defaultlistitem

---
+++ lib/models/default_list_item.dart
@@ -0,0 +1,9 @@
+class DefaultListItem {
+  const DefaultListItem({
+    this.text = '',
+    this.id = 0,
+  });
+
+  final String text;
+  final int id;
+}
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,4 @@
 export 'user.dart';
 export 'username.dart';
 export 'password.dart';
+export 'default_list_item.dart';
